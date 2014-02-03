e9cc2a76ba5be1b2c988f1aafc88a6740da78080
jvaccaro <jvaccaro@ubuntu.(none)>
2014-02-02 23:21:03 -0500

    forrest is pretty much the best

--- HW2/Ex6.1.py
+++ HW2/Ex6.1.py
@@ -7,3 +7,27 @@ Created on Sun Feb  2 22:59:52 2014
 
 "Exercise 6.1"
 
+"""
+Write a
+compare
+function that returns
+1
+if
+x > y
+,
+0
+if
+x == y
+, and
+-1
+if
+x < y"""
+
+def compare(x, y):
+    a = x - y
+    if a>0:
+        print 1
+    elif a == 0:
+        print 0
+    else:
+        print -1
